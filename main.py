b439a4de03d969952b38ce39cec48b0ee2c31e46
Luis <luis.borsoi@sap.com>
2022-10-23 23:24:06 -0300

    adjust pipeline config. Adjust port convertion to int

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from earnings_release.server import app
 
 if __name__ == "__main__":
     if os.getenv("PORT"):
-        app.run(host="0.0.0.0", port=os.getenv("PORT"))
+        app.run(host="0.0.0.0", port=int(os.getenv("PORT")))
     else:
         app.run(host="0.0.0.0", port=8000)
